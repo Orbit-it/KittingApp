1722fcc2cad71bc791b82ee53ed7c22838c0354d
Orbit-it <mansourdiop0011@gmail.com>
2024-05-20 14:44:17 +0100

    Jour 2

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
+    path('selected_poste', views.selected_poste, name='selected_poste'),
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
--- apps/home/views.py
+++ apps/home/views.py
@@ -4,6 +4,7 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django import template
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
@@ -34,6 +35,9 @@ def index(request):
             "poste": poste,
             "listing": listing,
                }
+    
+
+   
 
     html_template = loader.get_template('home/index.html')
     return HttpResponse(html_template.render(context, request))
@@ -67,3 +71,15 @@ def pages(request):
         html_template = loader.get_template('home/page-500.html')
         return HttpResponse(html_template.render(context, request))
 
+
+@login_required(login_url="/login/")
+def selected_poste(request):
+    if request.method == "POST":
+        id = request.POST.get('id')
+        try:
+            poste = Poste.objects.get(id=id)
+            # Effectuez des opérations avec l'objet `poste` selon vos besoins
+            return JsonResponse({"status": "success", "intitule_du_poste": poste.intitule, "projet_encours": poste.projet_id.intitule})
+        except Poste.DoesNotExist:
+            return JsonResponse({"status": "error", "message": "Poste not found."})
+    return JsonResponse({"status": "error", "message": "Invalid request method."})
